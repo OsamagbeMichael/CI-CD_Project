d1f12d8297f1af3b52dc04bbcdde2f7e26bb5dbf
OsamagbeMichael <uosamagbe@Gmail.com>
2025-01-29 07:35:14 -0500

    Test Terraform Plan workflow

--- variable.tf
+++ variable.tf
@@ -1,6 +1,6 @@
 variable "ci_cd_project" {
     description  = "ci_cd_project"
     type = string
-    default = "ci_cd_project"                   
+    default = "my-new-bucket-name"                   
 
 }
